cffac764ba56dc589febb6ef3cc7b9a77eb36d65
harshil-daynil <go4harshil@gmail.com>
2023-08-28 10:45:20 +0530

    audio error

--- android/app/src/main/java/com/chatapp/MainApplication.java
+++ android/app/src/main/java/com/chatapp/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.doublesymmetry.trackplayer.TrackPlayer;
 import com.reactnative.ivpusic.imagepicker.PickerPackage;
 import com.reactnative.ivpusic.imagepicker.PickerPackage;
 import com.facebook.react.ReactInstanceManager;
